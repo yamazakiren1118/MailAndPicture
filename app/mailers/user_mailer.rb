d69102526847aaa2f6b632662396e5e2f0104d7d
yamazakiren1118 <yamazakiren1118@yahoo.co.jp>
2020-10-29 00:06:41 +0900

    0/06

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def post_mail(user)
     @user = user
-    mail(from:'test@mail.com',to: @user[:email],subject: 'メールを送信')
+    mail(from:'yamazakiren1118@yahoo.co.jp',to: @user[:email],subject: 'メールを送信')
   end
 end
